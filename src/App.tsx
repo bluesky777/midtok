7cb952ac5a35039ad72ec16367675096ed0dd326
Joseth G <davidguerrero777@gmail.com>
2022-01-12 18:33:30 -0500

    feat(Root): Use FeedVideos component to show more than one video

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import FeedVideos from './components/FeedVideos'
 import "./App.css"
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App"> 
       <main>
-        Tiktok clon
+        <FeedVideos />
       </main>
     </div>
   )
